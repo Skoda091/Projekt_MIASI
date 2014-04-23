a18d71558a9dfdae713f7885a00a9dbecfb71224
Adam <vexhelel@gmail.com>
2014-04-23 18:05:19 +0200

    cos tam

--- src/game_window.rb
+++ src/game_window.rb
@@ -2,7 +2,7 @@ require 'gosu'
 
 class GameWindow < Gosu::Window
   def initialize
-    super 640, 480, false
+    super 800, 600, false
     self.caption = "Gosu Tutorial Game"
     @background_image = Gosu::Image.new(self, "../data/graphics/archer.jpg", true)
 
