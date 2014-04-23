8bff2b7c7e69a06a154f3242936d811c36ea5352
HarenBroog <damian.romanow@gmail.com>
2014-04-23 18:57:54 +0200

    Probny commit2

--- src/game_window.rb
+++ src/game_window.rb
@@ -1,21 +1,27 @@
 require 'gosu'
 
+load '../src/classes/unit.rb'
+
 class GameWindow < Gosu::Window
   def initialize
+    @z=0
     super 1600, 900, true
     self.caption = "Gosu Tutorial Game1"
     @background_image = Gosu::Image.new(self, "../data/graphics/archer.jpg", true)
+    @u=Unit.new(0,900,self,"left","swordsman")
 
   end
 
   def update
     if button_down? Gosu::KbEscape  then
       close
-      end
+    end
+    @u.move()
   end
 
   def draw
     @background_image.draw(0, 0, 0)
+    @u.draw()
   end
 end
 
