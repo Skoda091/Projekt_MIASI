1d9e78710d2b2b125ae2033b102d545d24bfb443
Adam <vexhelel@gmail.com>
2014-05-30 11:08:16 +0200

    symulacja konfliktów

--- src/classes/object.rb
+++ src/classes/object.rb
@@ -61,6 +61,8 @@ class Object
 
   def set_image anim
     @image = anim[anim.size-@cooldown_counter/(@cooldown_time/anim.size)]
+    # @image = anim[anim.count-1-@cooldown_counter/(@cooldown_time/anim.size)]
+
   end
 
 end
